781f92e2772cdb430702843153663c9b25917ffd
nik.kulik2001 <nik.kulik2001@mail.ru>
2020-11-29 16:22:41 +0300

    Lib

---
+++ Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ChessLib
+{
+
+    [Serializable]
+    public class ChessEventArgs
+    {
+        public int NewX { get; private set; }
+        public int NewY { get; private set; }
+
+
+        public int OldX { get; private set; }
+        public int OldY { get; private set; }
+        public ChessEventArgs(int newX, int newY, int oldX, int oldY)
+        {
+            NewX = newX;
+            NewY = newY;
+            OldX = oldX;
+            OldY = oldY;
+
+        }
+
+    }
+    public delegate void ChessEventHandler(object sender, ChessEventArgs e);
+
+}
